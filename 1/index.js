485642f2ffb1bbcee2db3376d3321305c320557e
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-08-08 07:10:11 -0700

    Add files via upload
    b-js1

---
+++ 1/index.js
@@ -0,0 +1,17 @@
+
+const num=require('./student');
+// console.log(num.num1());
+// console.log(num.age);
+// console.log(num.getName());
+
+//222222222222
+// console.log(num.getname());
+// console.log(num.getage());
+// console.log(num.cgp);
+
+
+
+// console.log(num.getname());
+// console.log(num.getname().number);
+// console.log(num.getname());
+console.log(num.getname()[1]);
---
+++ 1/student.js
@@ -0,0 +1,43 @@
+// const caLLnume=()=>{
+//    return 'tufan ali';
+// }
+
+// const age=24;
+
+// exports.num1=caLLnume;
+// exports.age=age;
+
+
+// 11111111111111111111
+// exports.getName=()=>{
+//    return 'M/s..Toli Ma'
+// };
+
+// 222222222222222
+// const getname=()=>{
+//    return'Tufan ali'
+// }
+// const getage=()=>{
+//    return 23;
+// }
+
+// const cgp=3.92;
+
+// module.exports={
+//    getname,
+//    getage,
+//    cgp
+// }
+// exports.getname=()=>{
+//  let num={
+//    number:45,
+//    nname:'toli'
+//  }
+//  return num;
+// }
+
+
+exports.getname=()=>{
+ let num=['toli',45,'ali']
+ return num;
+}
